f6d5e36b94c97624f7641a267e19feb8db128317
iKanavSharma <161232292+iKanavSharma@users.noreply.github.com>
2024-03-24 12:19:15 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (69.01%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,39 +1,10 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        int ind1=-1;
-        int ind2=-1;
-        // step 1 find breaking point 
-        for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind1=i;
-                break;
-            }
-        }
-        // if there is no breaking  point 
-        if(ind1==-1){
-            reverse(nums,0);
-        }
-        
-        else{
-            // step 2 find next greater element and swap with ind2
-            for(int i=nums.length-1;i>=0;i--){
-                if(nums[i]>nums[ind1]){
-                    ind2=i;
-                    break;
-                }
-            }
-
-            swap(nums,ind1,ind2);
-            // step 3 reverse the rest right half
-            reverse(nums,ind1+1);
-        }
-    }
-    void swap(int[] nums,int i,int j){
+    void swap(int nums[],int i,int j){
         int temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
     }
-    void reverse(int[] nums,int start){
+    void reverse(int nums[],int start){
         int i=start;
         int j=nums.length-1;
         while(i<j){
@@ -42,4 +13,28 @@ class Solution {
             j--;
         }
     }
+    public void nextPermutation(int[] nums) {
+        int indx1=-1;//index for break point
+        int indx2=-1;
+        int n=nums.length;
+        for(int i=n-2;i>=0;i--){//backward travesral for finding breaking point
+            if(nums[i]<nums[i+1]){
+                indx1=i;
+                break;
+            }
+        }
+        if(indx1==-1){
+            reverse(nums,0);//last permutation so next will be first array
+        }else{
+            for(int i=n-1;i>=0;i--){
+                if(nums[i]>nums[indx1]){
+                    indx2=i;
+                    break;
+                }
+            }
+            swap(nums,indx1,indx2);
+            reverse(nums,indx1+1);
+        }
+    }
 }
+       
