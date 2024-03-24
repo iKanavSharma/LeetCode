8f77f368d75b75dfa984a64f22055244a44dc6ce
iKanavSharma <161232292+iKanavSharma@users.noreply.github.com>
2024-03-24 16:47:55 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (69.01%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -14,27 +14,25 @@ class Solution {
         }
     }
     public void nextPermutation(int[] nums) {
-        int indx1=-1;//index for break point
-        int indx2=-1;
+        int indx=-1;//index for break point
         int n=nums.length;
         for(int i=n-2;i>=0;i--){//backward travesral for finding breaking point
             if(nums[i]<nums[i+1]){
-                indx1=i;
+                indx=i;
                 break;
             }
         }
-        if(indx1==-1){
+        if(indx==-1){
             reverse(nums,0);//last permutation so next will be first array
-        }else{
-            for(int i=n-1;i>=0;i--){
-                if(nums[i]>nums[indx1]){
-                    indx2=i;
-                    break;
-                }
-            }
-            swap(nums,indx1,indx2);
-            reverse(nums,indx1+1);
+            return;
         }
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[indx]){
+                swap(nums,i,indx);
+                break;
+            }
+        }
+        reverse(nums,indx+1);
     }
 }
        
