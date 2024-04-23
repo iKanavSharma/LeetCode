f5ccb99ec3eb269d93f2789cdfdf9b92e67bc754
iKanavSharma <161232292+iKanavSharma@users.noreply.github.com>
2024-04-23 22:48:41 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (38.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,24 +2,38 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        //helper array
-        int row[]=new int [m];
-        int col[]=new int [n];
+        int col=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
+                //row{0}=col[n];
+                //col{0}=row[m];
                 if(matrix[i][j]==0){
-                    //marking arrays
-                    row[i]=1;
-                    col[j]=1;
+                    matrix[i][0]=0;
+                    //edge case of overlapping
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }else{
+                        col=0;
+                    }
+                }  
+            }
+        }
+        //neglecting marked row and column 
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
                 }
             }
         }
-        //converting row and column to zero with the help of helper array
-        for(int i=0;i<m;i++){
+        if(matrix[0][0]==0){
             for(int j=0;j<n;j++){
-                if(row[i]==1 || col[j]==1){
-                    matrix[i][j]=0;
-                }
+                matrix[0][j]=0;
+            }
+        }
+        if(col==0){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
             }
         }
     }
