79865ddcea3c0fa24032ebcd46e0fa1510308b5d
iKanavSharma <161232292+iKanavSharma@users.noreply.github.com>
2024-04-26 20:18:29 +0530

    Time: 145 ms (56.92%), Space: 41.1 MB (94.68%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,43 +1,34 @@
-public class Solution {
+class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
+        int m=board.length;
+        int n=board[0].length;
+        boolean [][]visited=new boolean [m][n];
+        boolean result= false;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word.charAt(0)){
+                    result=backTrack(board,word,visited,i,j,0);
+                    if(result){
+                        return true;
+                    }
                 }
             }
         }
-        
         return false;
     }
-    
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
+    public boolean backTrack(char[][] board,String word,boolean [][]visited,int i,int j,int indx){
+        if(indx==word.length()){
             return true;
         }
-        
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+        //base case
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || visited[i][j] || board[i][j]!=word.charAt(indx)){
             return false;
         }
-        
-        visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
+        visited[i][j]=true;
+        if(backTrack(board,word,visited,i+1,j,indx+1) || backTrack(board,word,visited,i-1,j,indx+1 )|| backTrack(board,word,visited,i,j+1,indx+1) || backTrack(board,word,visited,i,j-1,indx+1)){
             return true;
         }
-        
-        visited[i][j] = false;
+        visited[i][j]=false;
         return false;
     }
 }
-
