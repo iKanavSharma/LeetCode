9c5801050d1fe61d515865b3761d40a2e8af22f3
iKanavSharma <161232292+iKanavSharma@users.noreply.github.com>
2024-06-11 23:31:34 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (49.02%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -16,16 +16,19 @@ class Solution {
             temp=temp.next;
             size++;
         }
+        //edge case
         if(n==size){
             return head.next;
         }
+        //type of pointer
         int i=0;
-        int itoFind=size-n-1;
+        int itoFind=size-n-1;//0 based indexing and to find that particular node from end
         ListNode prev=head;
         while(i<itoFind){
-            prev=prev.next;
+            prev=prev.next;//heave to skip
             i++;
         }
+        //to remove that node 
         prev.next=prev.next.next;
         return head;
     }
