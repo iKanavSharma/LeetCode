ffb0c94de16335bcce6b3a08b3067e7d53a832bf
iKanavSharma <161232292+iKanavSharma@users.noreply.github.com>
2024-06-17 22:32:20 +0530

    Time: 165 ms (5.12%), Space: 44.2 MB (58.60%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,33 +9,30 @@
  * }
  */
 class Solution {
-    //convert array to list
-    private ListNode convertToList(ArrayList<Integer> arr){
-        if(arr.isEmpty()){
-            return null;
+    
+    private ListNode mergetwoLists(ListNode list1,ListNode list2){
+        if(list1==null){
+            return list2;
         }
-        ListNode head=new ListNode(arr.get(0));
-        ListNode mover=head;
-        int n=arr.size();
-        for(int i=1;i<n;i++){
-            ListNode temp=new ListNode(arr.get(i));
-            mover.next=temp;
-            mover=temp;
+        if(list2==null){
+            return list1;
         }
-        return head;
+        if(list1.val<=list2.val){
+            ListNode res=list1;
+            list1.next=mergetwoLists(list1.next,list2);
+            return res;
+        }else{
+            ListNode res=list2;
+            list2.next=mergetwoLists(list1,list2.next);
+            return res;
+        }
     }
     public ListNode mergeKLists(ListNode[] lists) {
-        ArrayList<Integer> arr=new ArrayList<>();
-        int n=lists.length;
-        for(int i=0;i<n;i++){
-            ListNode temp=lists[i];
-            while(temp!=null){
-                arr.add(temp.val);
-                temp=temp.next;
-            }
+        ListNode res=null;
+        //for each loop
+        for(ListNode list:lists){
+            res=mergetwoLists(res,list);
         }
-        Collections.sort(arr);
-        ListNode head=convertToList(arr);
-        return head;
+        return res;
     }
 }
