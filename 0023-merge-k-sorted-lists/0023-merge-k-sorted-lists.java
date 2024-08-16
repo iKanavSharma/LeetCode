65cd90cf3958b88609ff5c9280baf090648360dd
iKanavSharma <161232292+iKanavSharma@users.noreply.github.com>
2024-08-16 22:35:30 +0530

    Time: 172 ms (5.12%), Space: 44.4 MB (50.71%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,29 +9,29 @@
  * }
  */
 class Solution {
-    
-    private ListNode mergetwoLists(ListNode list1,ListNode list2){
-        if(list1==null){
+    public ListNode mergeTwoList(ListNode list1,ListNode list2){
+        if(list1==null)
             return list2;
-        }
-        if(list2==null){
+        
+        if(list2==null)
             return list1;
-        }
+        
         if(list1.val<=list2.val){
             ListNode res=list1;
-            list1.next=mergetwoLists(list1.next,list2);
+            list1.next=mergeTwoList(list1.next,list2);
             return res;
-        }else{
+        }
+        else{
             ListNode res=list2;
-            list2.next=mergetwoLists(list1,list2.next);
+            list2.next=mergeTwoList(list1,list2.next);
             return res;
         }
     }
     public ListNode mergeKLists(ListNode[] lists) {
         ListNode res=null;
-        //for each loop
+        //for each loop for iteration
         for(ListNode list:lists){
-            res=mergetwoLists(res,list);
+            res=mergeTwoList(res,list);
         }
         return res;
     }
